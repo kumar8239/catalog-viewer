408f8008d2c4ffb64a3e9c5ff7f2895f909fa97d
kumar8239 <134601746+kumar8239@users.noreply.github.com>
2023-05-31 03:42:02 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Gallery from './components/Gallery';
+function App() {
+    return (
+    <div className="App">
+        <Gallery />
+    </div>
+    );
+}
+export default App;
